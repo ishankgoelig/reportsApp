16eed126f79c96f0438b880058c783eed57be6ae
Ishank Goel <ishank.goel28@gmail.com>
2025-08-05 19:37:39 +0530

    Remove unused imports

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -4,16 +4,9 @@ import { CommonModule } from '@angular/common';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports.component';
 
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { DeleteConfirmDialogComponent } from './components/delete-confirm-dialog/delete-confirm-dialog.component';
 
@@ -26,16 +19,10 @@ import { DeleteConfirmDialogComponent } from './components/delete-confirm-dialog
     CommonModule,
     ReportsRoutingModule,
     FormsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatIconModule,
     MatDialogModule,
     MatButtonModule,
-    MatToolbarModule
+    MatIconModule,
+    FormsModule
   ]
 })
 export class ReportsModule { }
